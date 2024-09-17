5af8da9942ff7c68e535193094780423c33885aa
Andres Coppola <coppola.andres@gmail.com>
2024-09-17 16:32:43 -0300

    first commit

---
+++ Eventubi.Api/Controllers/EventsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eventubi.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+
+        [HttpGet("")]
+        public async Task<IActionResult> Events()
+        {
+            var test = "this is a test";
+
+            return Ok(test);
+        }
+
+    }
+}
